669648ae853242d0c9a7018da86f1bf734a3e152
shubham <shubham.tripathi@ltts.com>
2024-05-15 14:44:06 +0530

    utility added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'isd',
+    version= '0.0.0',
+    author= 'Shubham',
+    author_email= 'tripathishubham960@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
